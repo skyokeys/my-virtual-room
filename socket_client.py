e318095329cd411b731e0fe5055179c2740c8567
skyoker <138794391+skyokeys@users.noreply.github.com>
2023-07-19 11:55:02 +0200

    Add files via upload

--- socket_client.py
+++ socket_client.py
@@ -5,18 +5,26 @@ server_port = 3141
 def connect_to_server():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_host, server_port))
-    print(f'connectet to server')
+    server_open = False
+    while not server_open:
+        print('Password:')
+        password = input()
+        if password == '3141592Pi':
+            print('correct :')
+            print('welcome skyoker')
+            server_open = True
+        else:
+            print('wrong password')
+    print("---------------Server---------------")
+    print('    - server host = 0.0.0.0')
+    print('    - server port = 3141')
+    print('ready to send commands!')
     exit = False
     while not exit:
         command = input()
         client_socket.send(command.encode('utf-8'))
         output = client_socket.recv(1024).decode('utf-8')
         print(output)
-        if command == 'start server':
-            print("---------------Server---------------")
-            print('    - server host = 0.0.0.0')
-            print('    - server port = 3141')
-            print('ready to send commands!')
         if command == 'close server':
             client_socket.close
             exit = True
